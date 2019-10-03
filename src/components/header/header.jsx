671dcfdfcfa1f439150d690a8a44d735eaa15cba
Rachel Dotey <rachel.dotey@gmail.com>
2019-10-03 16:22:33 -0400

    Header menu and styling
    Added basic header styling. Added a call to get the menu for location header-menu and display it using bootstrap.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,42 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
-import './header.scss';
+import "./header.scss";
 
-const Header = () => (
-  <div className="container">
-    <header id="masthead" className="site-header" role="banner">
-      <nav className="navbar navbar-expand-lg navbar-light ">
-        <h1 className="site-title">
-          <Link to={ThemeVariables.path}>Rachel Dotey</Link>
-        </h1>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link className="nav-item nav-link active" to={ThemeVariables.path}>
-              Home <span className="sr-only">(current)</span>
-            </Link>
-            <Link
-              className="nav-item nav-link"
-              to={ThemeVariables.path + "products/"}
-            >
-              Products
-            </Link>
-          </div>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headerMenu: [
+        {
+          ID: 0,
+          order: 1,
+          parent: 0,
+          title: "Home",
+          url: ThemeVariables.URL.root,
+          target: "",
+          classes: "",
+          type: "page",
+          children: []
+        }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    this.getHeaderMenu();
+  }
+
+  getHeaderMenu() {
+    fetch(ThemeVariables.URL.apitheme + "menu/header-menu")
+      .then(res => res.json())
+      .then(results => {
+        this.setState({ headerMenu: results });
+      })
+      .catch(error => {
+        console.log(
+          "There has been a problem with your fetch operation: " + error.message
+        );
+      });
+  }
+
+  render() {
+    return (
+      <header className="site-header" role="banner">
+        <div className="container">
+          <Navbar variant="dark">
+            <Navbar.Brand href={ThemeVariables.URL.root} className="site-title">
+              {ThemeVariables.title}
+            </Navbar.Brand>
+            <Nav className="mr-auto site-nav">
+              {this.state.headerMenu.map(item => (
+                <Nav.Link href={item.url} key={item.ID}>
+                  {item.title}
+                </Nav.Link>
+              ))}
+            </Nav>
+          </Navbar>
         </div>
-      </nav>
-    </header>
-  </div>
-);
+      </header>
+    );
+  }
+}
 
 export default Header;
