2295df73c8bf57f53779ada0e6053a92a13c7be6
Rachel Dotey <rachel.dotey@gmail.com>
2019-10-05 13:53:55 -0400

    Create slogan-rotator.jsx
    Created a very simple text rotator that cycles through an array of strings every few seconds and instantly cycles when clicked.

---
+++ src/components/slogan-rotator/slogan-rotator.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+class SloganRotator extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+
+    this.state = {
+      class: props.className ? props.className : "slogan-rotator",
+      displayID: props.displayID ? parseInt(props.displayID) : 0,
+      milliseconds: props.seconds ? (parseInt(props.seconds)*1000) : 3000,
+      slogans: props.sloganArray
+        ? props.sloganArray
+        : ["Slogan array not provided.", "Click me!", "Property slogan array not set."]
+    };
+  }
+
+  componentDidMount() {
+    this.startSloganRotator();
+  }
+
+  componentWillUnmount() {
+    this.stopSloganRotator();
+  }
+
+  startSloganRotator = () => {
+    this.timer = setInterval(this.rotateSlogan, this.state.milliseconds);
+  };
+
+  stopSloganRotator = () => {
+    clearInterval(this.timer);
+  };
+
+  rotateSlogan = () => {
+    let i = this.state.displayID+1;
+    this.setState({displayID: (i >= this.state.slogans.length) ? 0 : i});
+    console.log(this.state);
+  };
+
+  sloganClickEvent = () => {
+    this.stopSloganRotator();
+    this.rotateSlogan();
+    this.startSloganRotator();
+  };
+
+  render() {
+    return (
+      <span className={this.state.class} onClick={this.sloganClickEvent}
+      dangerouslySetInnerHTML={{
+        __html: this.state.slogans[this.state.displayID]
+      }}></span>
+    );
+  }
+}
+
+export default SloganRotator;
